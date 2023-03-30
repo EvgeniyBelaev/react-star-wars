08d7d9871201860a038ac2bd7aa16eb7cf4a74d9
Evgeniy Belaev <ekjek@mail.ru>
2023-03-31 00:35:39 +0300

    Favorite page done

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 import icon from './img/bookmark.svg'
 
 import style from './Favorite.module.css';
 
 const Favorite = () => {
+    const [count, setCount] = useState(0)
+    const storeData = useSelector(state => state.favoriteReducer)
+
+    useEffect(() => {
+        const length = Object.keys(storeData).length
+        length.toString().length > 2 ? setCount('...') : setCount(length)
+    })
+
     return (
-        <div>
+        <div className={style.container}>
             <Link to="/favorites">
-                <span className={style.counter}>0</span>
+                <span className={style.counter}>{count}</span>
                 <img src={icon} alt="icon" className={style.icon} />
             </Link>
             
--- src/containers/FavoritesPage/FavoritesPage.jsx
+++ src/containers/FavoritesPage/FavoritesPage.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux'
+
+import PepleList from '@components/PeoplePage/PeopleList'
+
 import style from './FavoritesPage.module.css';
 
 const FavoritesPage = () => {
+    const [people, setPeople] = useState([])
 
     const storeData = useSelector(state => state.favoriteReducer)
 
+    useEffect(() => {
+        const arr = Object.entries(storeData)
+
+        if(arr.length) {
+            const res = arr.map(item => {
+                return {
+                    id: item[0],
+                    ...item[1]
+                }
+            })
+
+            setPeople(res)
+        }
+    }, [])
+
     
     return (
-        <h1>FavoritesPage</h1>
+        <>
+            <h1 className='header__text'>Favorite Page</h1>
+            {people.length
+                ? <PepleList people={people} />
+                : <h2 className={style.comment}>No data</h2>
+            }
+            
+        </>
     );
 }
 
