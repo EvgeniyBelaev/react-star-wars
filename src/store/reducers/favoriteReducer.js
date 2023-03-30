841dfbdf39111113da2878535de59065700e93c9
Evgeniy Belaev <ekjek@mail.ru>
2023-03-31 01:30:43 +0300

    localStorage

--- src/store/reducers/favoriteReducer.js
+++ src/store/reducers/favoriteReducer.js
@@ -1,7 +1,8 @@
 import { omit } from 'lodash'
+import { getLocalStorage } from '@utils/localStorage'
 import { ADD_PERSON_TO_FAVORITE, REMOVE_PERSON_FROM_FAVORITE } from '@store/constants/actionTypes'
 
-const initialState = {}
+const initialState = getLocalStorage('store')
 
 const favoriteReducer = (state = initialState, action) => {
     switch (action.type) {
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import { setLocalStorage } from '@utils/localStorage'
 
 import reducer from './reducers'
 
 const store = configureStore({
     reducer
 
+})
+
+store.subscribe(() => {
+
+    setLocalStorage('store', store.getState().favoriteReducer)
 })
 
 export default store
